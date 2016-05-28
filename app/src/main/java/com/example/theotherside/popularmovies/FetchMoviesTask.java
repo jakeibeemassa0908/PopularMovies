b8e5674139d8cb23800111757f6fe4d7ed04ec7d
jakeibeemassa <jac.massa0908@gmail.com>
2016-05-27 23:34:50 -0500

    create movie class and return movie array from asynctask

--- app/src/main/java/com/example/theotherside/popularmovies/FetchMoviesTask.java
+++ app/src/main/java/com/example/theotherside/popularmovies/FetchMoviesTask.java
@@ -4,24 +4,31 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created by theotherside on 5/27/16.
  */
-public class FetchMoviesTask extends AsyncTask<String, Void, Void> {
+public class FetchMoviesTask extends AsyncTask<String, Void, ArrayList> {
 
     private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
 
     @Override
-    protected Void doInBackground(String... params) {
+    protected ArrayList<Movie> doInBackground(String... params) {
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
+
+        ArrayList<Movie> movies = null;
 
 
         String listType = params[0];
@@ -60,10 +67,13 @@ public class FetchMoviesTask extends AsyncTask<String, Void, Void> {
             }
 
             movieListStr = buffer.toString();
+            movies =getMovieDataFromJson(movieListStr);
 
             Log.v(LOG_TAG, movieListStr);
 
         } catch (IOException e) {
+            Log.e(LOG_TAG, "Error", e);
+        } catch (JSONException e) {
             Log.e(LOG_TAG, "Error", e);
         } finally {
             if (urlConnection != null) {
@@ -78,6 +88,40 @@ public class FetchMoviesTask extends AsyncTask<String, Void, Void> {
             }
         }
 
-        return null;
+        return movies;
+    }
+
+    private ArrayList<Movie> getMovieDataFromJson(String moviesJsonStr) throws JSONException {
+        ArrayList<Movie> movies = new ArrayList<>();
+
+        //name of the JSON objects that needs to be extracted
+        final String RESULTS="results";
+        final String TITLE = "original_title";
+        final String IMAGE ="backdrop_path";
+        final String OVERVIEW ="overview";
+        final String RATING = "vote_average";
+        final String RELEASE_DATE ="release_date";
+
+        JSONObject moviesJson = new JSONObject(moviesJsonStr);
+        JSONArray movieArray = moviesJson.getJSONArray(RESULTS);
+
+        for (int i =0;i <movieArray.length();i++){
+
+
+            JSONObject movie = movieArray.getJSONObject(i);
+
+            Movie movieData = new Movie();
+
+            movieData.setTitle(movie.getString(TITLE));
+            movieData.setImage(movie.getString(IMAGE));
+            movieData.setOverview(movie.getString(OVERVIEW));
+            movieData.setRating(movie.getString(RATING));
+            movieData.setReleaseDate(movie.getString(RELEASE_DATE));
+
+
+            Log.v(LOG_TAG,movieData.getTitle());
+
+        }
+        return movies;
     }
 }
---
+++ app/src/main/java/com/example/theotherside/popularmovies/Movie.java
@@ -0,0 +1,49 @@
+package com.example.theotherside.popularmovies;
+
+/**
+ * Created by theotherside on 5/27/16.
+ */
+public class Movie {
+
+    private String title,image,overview,rating,releaseDate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
